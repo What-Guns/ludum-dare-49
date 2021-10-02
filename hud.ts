31a47fbf911dd7348563cddf132ced35d8c16735
bassguitarbill <bassguitarbill@gmail.com>
2021-10-01 21:35:25 -0400

    Make HUD actually invisible

--- hud.ts
+++ hud.ts
@@ -1,17 +1,18 @@
 class HudWindow {
   private element: HTMLElement;
-  private visible: boolean;
+  private visible: boolean = false;
   x = 0;
   y = 0;
 
   constructor() {
     this.element = document.createElement('div');
+    this.element.style.visibility = 'hidden';
+    this.visible = false;
     this.element.style.position = 'absolute';
     this.element.style.left = String(this.x);
     this.element.style.right = String(this.y);
     this.element.innerText = 'Hello';
     this.element.classList.add('hudWindow');
-    this.visible = true;
     document.body.appendChild(this.element);
   }
 
