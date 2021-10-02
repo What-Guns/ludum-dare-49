670a49a085aafbeb1b79e9299e62c0b9c465f8f1
Chris Mondok <chris.mondok@gmail.com>
2021-10-02 16:51:26 -0400

    cauldron is resizeable

--- cauldron.ts
+++ cauldron.ts
@@ -68,6 +68,11 @@ export class Cauldron implements Thing {
     ctx.fillStyle = 'black';
     ctx.fillRect(this.x - this.width/2, this.y - this.height/2, this.width, this.height);
   }
+
+  debugResize(evt: WheelEvent) {
+    this.width += evt.deltaX / 50;
+    this.height -= evt.deltaY / 50;
+  }
 }
 
 interface CauldronData {
