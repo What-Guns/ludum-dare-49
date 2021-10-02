58de2b73dbb039e043fe307f608658a346191f29
Chris Mondok <chris.mondok@gmail.com>
2021-10-01 20:37:01 -0400

    move a lot of stuff out of main

--- main.ts
+++ main.ts
@@ -1,30 +1,12 @@
+import {Player} from './player.js';
+import {pointer} from './input.js';
+
 addEventListener('load', () => {
   const canvas = document.querySelector('canvas')!;
   const game = new Game(canvas);
 
   requestAnimationFrame(main);
 
-  canvas.addEventListener('touchstart', evt => {
-    evt.preventDefault();
-    game.touched(evt);
-  });
-
-  canvas.addEventListener('touchmove', evt => {
-    evt.preventDefault();
-    game.touched(evt);
-  });
-
-  canvas.addEventListener('mousedown', evt => {
-    game.mouseClicked(evt);
-  });
-
-  canvas.addEventListener('mousemove', evt => {
-    game.mouseMoved(evt);
-  })
-
-  canvas.addEventListener('mouseup', evt => {
-    game.mouseUnclicked(evt);
-  });
 
   addEventListener('resize', resizeCanvas);
 
@@ -48,23 +30,9 @@ addEventListener('load', () => {
   }
 });
 
-interface Pointer {
-  identifier: number;
-  x: number;
-  y: number;
-  active: boolean;
-}
-
 class Game {
   private room: Room;
   private ctx: CanvasRenderingContext2D;
-
-  pointer: Pointer = {
-    identifier: -1,
-    x: 0,
-    y: 0,
-    active: false,
-  };
 
   constructor(canvas: HTMLCanvasElement) {
     this.ctx = canvas.getContext('2d')!;
@@ -78,56 +46,12 @@ class Game {
     const frameRate = 1000/dt;
     this.ctx.fillText(frameRate.toFixed(0), 10, 10);
 
-    if(this.pointer.active) {
-      this.ctx.fillRect(this.pointer.x - 4, this.pointer.y - 1, 8, 2);
-      this.ctx.fillRect(this.pointer.x - 1, this.pointer.y - 4, 2, 8);
+    if(pointer.active) {
+      this.ctx.fillRect(pointer.x - 4, pointer.y - 1, 8, 2);
+      this.ctx.fillRect(pointer.x - 1, pointer.y - 4, 2, 8);
     }
   }
 
-  touched(evt: TouchEvent) {
-    const touch = this.getRelevantTouch(evt);
-    if(!touch) return;
-    this.pointer.identifier = touch.identifier;
-    this.pointer.x = touch.pageX;
-    this.pointer.y = touch.pageY;
-    this.pointer.active = true;
-  }
-
-  touchend(evt: TouchEvent) {
-    if(this.getRelevantTouch(evt)) {
-      this.pointer.active = false;
-    }
-  }
-
-  mouseClicked(evt: MouseEvent) {
-    this.pointer.active = true;
-    this.pointer.x = evt.pageX;
-    this.pointer.y = evt.pageY;
-    this.pointer.identifier = -1;
-  }
-
-  mouseMoved(evt: MouseEvent) {
-    this.pointer.x = evt.pageX;
-    this.pointer.y = evt.pageY;
-  }
-
-  mouseUnclicked(evt: MouseEvent) {
-    this.pointer.active = false;
-    this.pointer.x = evt.pageX;
-    this.pointer.y = evt.pageY;
-    this.pointer.identifier = -1;
-  }
-
-  private getRelevantTouch(evt: TouchEvent) {
-    if(!this.pointer.active) return evt.changedTouches.item(0);
-    for(let i = 0; i < evt.changedTouches.length; i++) {
-      const touch = evt.changedTouches.item(i);
-      if(!touch) return;
-      if(touch.identifier === this.pointer.identifier) return touch;
-    }
-
-    return null;
-  }
 }
 
 class Room {
@@ -145,13 +69,4 @@ interface Thing {
   tick?(dt: number): void;
 }
 
-class Player {
-  constructor(public x: number, public y: number) {}
-
-  draw(ctx: CanvasRenderingContext2D) {
-    const width = 32;
-    const height = 48;
-    ctx.fillRect(this.x - width/2, this.y - height, width, height);
-  }
-}
 
