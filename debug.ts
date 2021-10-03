fdba0fdb067a0eb551013b02d328819b62a7ea3a
Chris Mondok <chris.mondok@gmail.com>
2021-10-02 20:16:24 -0400

    Add menu option to spawn new spawners.

--- debug.ts
+++ debug.ts
@@ -1,11 +1,18 @@
 import {pointer} from './input.js';
 import {Thing} from './main.js';
+import {materials} from './material.js';
+import {deserialize} from './serialization.js';
 
 export let debug = false;
 export let held: Held|null = null;
 
 addEventListener('keydown', evt => {
   if(evt.key === '`') debug = !debug;
+  if(debug) {
+    document.body.classList.add('debug');
+  } else {
+    document.body.classList.remove('debug');
+  }
   held = null;
 });
 
@@ -38,4 +45,33 @@ export function debugTick() {
     held.previousPointer.x = pointer.x;
     held.previousPointer.y = pointer.y;
   }
+}
+
+const debugFunctions = {
+  async addResourceSpawner() {
+    const data = {
+      "@type": "ResourceSpawner",
+        "@path": "./resource-spawner.js",
+        "x": window.game!.room!.vanishingPoint.x,
+        "y": window.game!.room!.vanishingPoint.y,
+        "resourceType": resourceSelector!.value,
+    };
+    const spawner = await deserialize(data);
+    window.game!.room!.things.push(spawner);
+  }
+};
+
+for(const btn of Array.from(document.querySelectorAll('[data-debug-click]'))) {
+  const fnName = btn.getAttribute('data-debug-click') as keyof typeof debugFunctions;
+  const fn = debugFunctions[fnName];
+  if(!fn) throw new Error(`No debug function named ${fnName}`);
+  btn.addEventListener('click', fn);
+}
+
+const resourceSelector = document.getElementById('resource-selector') as HTMLSelectElement;
+for(const material of Object.keys(materials)) {
+  const option = document.createElement('option');
+  option.textContent = option.value = material;
+  option.selected = true;
+  resourceSelector.appendChild(option);
 }
