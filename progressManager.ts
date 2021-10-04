acbb1f1153c0cc81ebd9695b7515d28237bdf802
bassguitarbill <bassguitarbill@gmail.com>
2021-10-04 16:07:31 -0400

    Rutabaga dialog

--- progressManager.ts
+++ progressManager.ts
@@ -5,78 +5,91 @@ export const progressData = [
     name: 'initial',
     catDialog: 'Welcome to the cottage! Try, idk, picking things up.',
     ghostDialog: 'You definitely should not be in the attic yet',
+    rutabagaDialog: 'I feel like you cheated to get here. Shame!',
     unlockedRooms: ['foyer', 'greenhouse', 'hall', 'locked']
   },
   {
     name: 'got-key',
     catDialog: 'That key looks right, but it\'s far too large! You might need to apply a potion to the key to make it the correct size...',
     ghostDialog: 'I\'m serious dude, get out of the attic',
+    rutabagaDialog: 'I feel like you cheated to get here. Shame!',
     unlockedRooms: ['foyer', 'greenhouse', 'hall']
   },
   {
     name: 'unlocked-bottom-cabinet',
     catDialog: 'Well done! The other keyhole is far larger than the original key, but I think there\'s another potion that could help you. You can take another regular key if you need to!',
     ghostDialog: 'I\'m serious dude, get out of the attic',
+    rutabagaDialog: 'I feel like you cheated to get here. Shame!',
     unlockedRooms: ['foyer', 'greenhouse', 'hall']
   },
   {
     name: 'unlocked-top-cabinet',
     catDialog: 'The cupboard is fully unlocked, I\'m impressed! The cellar should be open now, try going down there and procuring a hushroom or two.',
     ghostDialog: 'I\'m serious dude, get out of the attic',
+    rutabagaDialog: 'I feel like you cheated to get here. Shame!',
     unlockedRooms: ['foyer', 'greenhouse', 'hall', 'cellar']
   },
   {
     name: 'destroyed-hushroom',
     catDialog: 'Hushrooms can\'t withstand loud music, they crumble away! Maybe you could create a potion to toughen it up temporarily?',
     ghostDialog: 'I\'m serious dude, get out of the attic',
+    rutabagaDialog: 'I feel like you cheated to get here. Shame!',
     unlockedRooms: ['foyer', 'greenhouse', 'hall', 'cellar']
   },
   {
     name: 'recovered-hushroom',
     catDialog: 'Good thinking! Now you\'ll have hushrooms available in the hall whenever you want!',
     ghostDialog: 'I\'m serious dude, get out of the attic',
+    rutabagaDialog: 'I feel like you cheated to get here. Shame!',
     unlockedRooms: ['foyer', 'greenhouse', 'hall', 'cellar']
   },
   {
     name: 'got-radio',
     catDialog: 'Please don\'t be scared of the ghost, they\'re just grumpy. I wonder what\'s wrong with their radio?',
     ghostDialog: 'I\'m serious dude, get out of the attic',
+    rutabagaDialog: 'I feel like you cheated to get here. Shame!',
     unlockedRooms: ['foyer', 'greenhouse', 'hall', 'cellar']
   },
   {
     name: 'fixed-radio',
     catDialog: 'That looks like it did the trick! That radio should be back in working condition, the ghost will be overjoyed!',
     ghostDialog: 'I\'m serious dude, get out of the attic',
+    rutabagaDialog: 'I feel like you cheated to get here. Shame!',
     unlockedRooms: ['foyer', 'greenhouse', 'hall', 'cellar']
   },
   {
     name: 'returned-radio',
     catDialog: 'To finish your potion, you\'ll need a Gravity Stone. There are some in the cellar, but it\'ll be tough to lift out of there...',
     ghostDialog: 'welcome to the spooooooooky attic, ha ha i mean its spooky because i live here, can i interest you in some ghost tears',
+    rutabagaDialog: 'I feel like you cheated to get here. Shame!',
     unlockedRooms: ['foyer', 'greenhouse', 'hall', 'cellar', 'attic']
   },
   {
     name: 'dropped-gravity-stone',
     catDialog: 'No luck? I think you\'ll need to use the furnace down there; certain potions only work on really, really hot items',
     ghostDialog: 'do you want more of my tears, no no i assure you its no trouble',
+    rutabagaDialog: 'I feel like you cheated to get here. Shame!',
     unlockedRooms: ['foyer', 'greenhouse', 'hall', 'cellar', 'attic']
   },
   {
     name: 'heated-gravity-stone',
     catDialog: 'I know there\'s a potion you can make to make that hot rock as light as a feather! If you make it, the rock should be no trouble.',
     ghostDialog: 'do you want more of my tears, no no i assure you its no trouble',
+    rutabagaDialog: 'I feel like you cheated to get here. Shame!',
     unlockedRooms: ['foyer', 'greenhouse', 'hall', 'cellar', 'attic']
   },
   {
     name: 'heated-floating-gravity-stone',
     catDialog: 'Once the gravity stone is floating, put it back in the furnace and it should float up the flue!',
     ghostDialog: 'do you want more of my tears, no no i assure you its no trouble',
+    rutabagaDialog: 'I feel like you cheated to get here. Shame!',
     unlockedRooms: ['foyer', 'greenhouse', 'hall', 'cellar', 'attic']
   },
   {
     name: 'recovered-gravity-stone',
     catDialog: 'That\'s fantastic! You should now have all of the ingredients you need to finish the last potion on the list!',
     ghostDialog: 'do you want more of my tears, no no i assure you its no trouble',
+    rutabagaDialog: 'I feel like you cheated to get here. Shame!',
     unlockedRooms: ['foyer', 'greenhouse', 'hall', 'cellar', 'attic']
   },
   {
@@ -117,5 +130,5 @@ export function isRoomUnlocked(roomName: string): boolean {
 
 export function getDialog(npcType: NpcType) {
   const currentLevelData = progressData[currentLevel];
-  return npcType === 'CAT' ? currentLevelData.catDialog :  npcType === 'RUTABAGA' ? currentLevelData.rutabagaDialog : currentLevelData.ghostDialog;
+  return npcType === 'CAT' ? currentLevelData.catDialog : npcType === 'RUTABAGA' ? currentLevelData.rutabagaDialog : currentLevelData.ghostDialog;
 }
